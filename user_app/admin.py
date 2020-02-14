9dc2fe0c3b215483f5d558f53abdce017000af90
Norman Carl Freudenberg <kound@posteo.de>
2020-02-14 22:10:19 +0100

    improve admin

--- user_app/admin.py
+++ user_app/admin.py
@@ -1,10 +1,13 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.utils.translation import ugettext_lazy as _
+from improved_user.forms import UserChangeForm, UserCreationForm
 
 from .models import User
 
 
 @admin.register(User)
-class UserAdmin(admin.ModelAdmin):
+class UserAdmin(BaseUserAdmin):
     list_display = (
         "short_name",
         "full_name",
@@ -23,4 +26,31 @@ class UserAdmin(admin.ModelAdmin):
         "date_joined",
         "is_rowo",
     )
-    readonly_fields = ("password",)
+    fieldsets = (
+        (None, {"fields": ("email", "password")}),
+        (_("Personal info"), {"fields": ("short_name", "full_name")}),
+        (
+            _("Permissions"),
+            {
+                "fields": (
+                    "is_active",
+                    "is_staff",
+                    "is_rowo",
+                    "is_superuser",
+                    "groups",
+                    "user_permissions",
+                ),
+            },
+        ),
+        (_("Important dates"), {"fields": ("last_login", "date_joined")}),
+    )
+    add_fieldsets = (
+        (None, {"classes": ("wide",), "fields": ("email", "password1", "password2"),}),
+    )
+
+    def get_fieldsets(self, request, obj=None):
+        return [(None, {"fields": self.get_fields(request, obj)})]
+
+    ordering = ("short_name",)
+    form = UserChangeForm
+    add_form = UserCreationForm
