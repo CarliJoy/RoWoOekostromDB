f04886fb7b7e1c665843373d95716e37cf43b247
Norman Carl Freudenberg <kound@posteo.de>
2020-02-14 21:46:17 +0100

    add admin for user app

--- user_app/admin.py
+++ user_app/admin.py
@@ -1,1 +1,26 @@
-# Register your models here.
+from django.contrib import admin
+
+from .models import User
+
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = (
+        "short_name",
+        "full_name",
+        "email",
+        "is_superuser",
+        "is_staff",
+        "is_active",
+        "date_joined",
+        "last_login",
+    )
+    list_filter = (
+        "last_login",
+        "is_superuser",
+        "is_staff",
+        "is_active",
+        "date_joined",
+        "is_rowo",
+    )
+    readonly_fields = ("password",)
