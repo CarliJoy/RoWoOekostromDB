a18ef68e3ed29cc3033997a58446c5f6a59d5ba2
Carli* Freudenberg <kound@posteo.de>
2024-10-09 19:05:49 +0200

    Add startpage url

--- oekostrom_db/anbieter/views.py
+++ oekostrom_db/anbieter/views.py
@@ -1,1 +1,12 @@
-# Create your views here.
+from django.http import HttpRequest, HttpResponse
+from django.shortcuts import render
+
+from .models import Anbieter
+
+
+def startpage(request: HttpRequest) -> HttpResponse:
+    context = {
+        "count_active": Anbieter.objects.filter(active=True).count(),
+    }
+
+    return render(request, "anbieter/startpage.html", context)
