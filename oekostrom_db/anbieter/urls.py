ad2f7d02acd493bf9c876cf764e12fb4fc9bc0c2
Carli* Freudenberg <kound@posteo.de>
2024-10-20 17:27:00 +0200

    fix view import

--- oekostrom_db/anbieter/urls.py
+++ oekostrom_db/anbieter/urls.py
@@ -1,9 +1,7 @@
 from django.conf import settings
 from django.urls import path
 
-import oekostrom_db.anbieter.view_mirror
-
-from . import views
+from . import view_mirror, views
 
 urlpatterns = [
     path("", views.startpage, name="startpage"),
@@ -15,13 +13,13 @@ if settings.ROWO_MIRRORING:
     urlpatterns += [
         path(
             "mirror/<path:file_path>",
-            oekostrom_db.anbieter.view_mirror.mirror,
+            view_mirror.mirror,
             name="mirror",
         ),
         # Handle themes special because we can't change some of the url
         path(
             "themes/<path:file_path>",
-            oekostrom_db.anbieter.view_mirror.theme,
+            view_mirror.theme,
             name="themes",
         ),
     ]
